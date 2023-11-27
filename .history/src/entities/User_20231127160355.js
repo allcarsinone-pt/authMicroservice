c6e326190978fd5b8123c45db18b19a48cdfdc73
pestevao <pestevao@sdilab.pt>
2023-11-27 17:00:07 +0000

    Ajustes pedidos pelo lindt

--- .history/src/entities/User_20231127160355.js
+++ .history/src/entities/User_20231127160355.js
@@ -6,7 +6,7 @@ class User {
      * @param {*} password password of user
      * @param {*} id id of user, can be a integer, an uuid or a string
      */
-  constructor (user) {
+  constructor ( user ) {
     this.id = user.id
     this.username = user.username
     this.name = user.name
@@ -19,6 +19,7 @@ class User {
     this.role = user.role
   }
 
+  
   toJson () {
     return { id: this.id, username: this.username, name: this.name, email: this.email, password: this.password, address: this.address, city: this.city, postalcode:this.postalcode, mobilephone: this.mobilephone, role: this.role }
   }
@@ -31,7 +32,7 @@ class User {
    * @param {*} id Id of User
    * @returns a new instance of User
    */
-  static create (user) {
+  static create ( user ) {
     if (!user) {
       throw new Error('Invalid user')
     }
@@ -43,12 +44,10 @@ class User {
     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) === false) {
       throw new Error('Invalid email')
     }
-    if (username.length < 6) {
+    if( username.length < 6 )
       throw new Error('Username is required');
-    }
-    if (role.length === 0) {
+    if( role.length === 0 )
       throw new Error('Role is required')
-    }
 
     return new User(user)
   }
@@ -60,27 +59,25 @@ class User {
    * @param {*} username Username of User
    * @returns a new instance of User
    */
-  static edit (user) {
-    if (!user) {
-      throw new Error('Invalid user')
+    static edit ( user ) {
+      if (!user) {
+        throw new Error('Invalid user')
+      }
+      const { username, name, email, role } = user;
+  
+      if (name.length === 0) {
+        throw new Error('Name is required')
+      }
+      if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) === false) {
+        throw new Error('Invalid email')
+      }
+      if( username.length < 6 )
+        throw new Error('Username is required and must contain at least 6 characters');
+      if( role.length === 0 )
+        throw new Error('Role is required')
+  
+      return new User(user)
     }
-    const { username, name, email, role } = user;
-
-    if (name.length === 0) {
-      throw new Error('Name is required')
-    }
-    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) === false) {
-      throw new Error('Invalid email')
-    }
-    if (username.length < 6) {
-      throw new Error('Username is required and must contain at least 6 characters');
-    }
-    if (role.length === 0) {
-      throw new Error('Role is required')
-    }
-
-    return new User(user)
-  }
 
   /**
    * @description Edit User making the necessary validations
@@ -89,11 +86,11 @@ class User {
    * @param {*} username Username of User
    * @returns a new instance of User
    */
-  static delete (user) {
+  static delete ( user ) {
     if (!user) {
       throw new Error('Invalid user')
     }
-
+    
     return new User(user)
   }
 }
