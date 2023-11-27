057fca1bb304c2ba4fff173df1ecba8b7f0c30b7
pestevao <pestevao@sdilab.pt>
2023-11-27 16:41:58 +0000

    ...

--- .history/src/repositories/PostgreUserRepository_20231127163317.js
+++ .history/src/repositories/PostgreUserRepository_20231127163317.js
@@ -6,21 +6,21 @@ class PostgreUserRepository {
   }
 
   async create (user) {
-    const { username, name, email, password, address, city, postalcode, mobilephone } = user
+    let { username, name, email, password, address, city, postalcode, mobilephone, role } = user
 
     const client = new pg.Client(this.baseURI)
     await client.connect()
-
+    
     const roleExists = await client.query('SELECT * FROM roles WHERE name = $1', [user.role])
-    let roleId
+    let roleId = undefined
     if (roleExists.rows.length === 0) {
-      console.log('role not exists')
-      const role = await client.query('INSERT INTO roles (name) VALUES ($1) RETURNING *', [user.role])
-      roleId = role.rows[0].id
+        console.log('role not exists')
+        const role = await client.query('INSERT INTO roles (name) VALUES ($1) RETURNING *', [user.role])
+        roleId = role.rows[0].id
     } else {
-      console.log('role exists')
-      roleId = roleExists.rows[0].id
-    }
+        console.log('role exists')
+        roleId = roleExists.rows[0].id
+    }  
 
     const query = 'INSERT INTO users ( username, name, password, address, city, postalcode, ' +
     'mobilephone, email, role_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *';
@@ -35,21 +35,21 @@ class PostgreUserRepository {
   }
 
   async edit (user) {
-    const { id, username, name, address, city, postalcode, mobilephone, email } = user
+    let { id, username, name, address, city, postalcode, mobilephone, email, role } = user
 
     const client = new pg.Client(this.baseURI)
     await client.connect()
-
+    
     const roleExists = await client.query('SELECT * FROM roles WHERE name = $1', [user.role])
-    let roleId
+    let roleId = undefined
     if (roleExists.rows.length === 0) {
-      console.log('Role does not exist')
-      const role = await client.query('INSERT INTO roles (name) VALUES ($1) RETURNING *', [user.role])
-      roleId = role.rows[0].id
+        console.log('Role does not exist')
+        const role = await client.query('INSERT INTO roles (name) VALUES ($1) RETURNING *', [user.role])
+        roleId = role.rows[0].id
     } else {
-      console.log('Role exists')
-      roleId = roleExists.rows[0].id
-    }
+        console.log('Role exists')
+        roleId = roleExists.rows[0].id
+    }  
 
     const query = 'UPDATE users SET  username = COALESCE($1, username), ' +
     'name = COALESCE($2, name), ' +
@@ -60,7 +60,7 @@ class PostgreUserRepository {
     'email = COALESCE($7, email), ' +
     'role_id = COALESCE($8, id) ' +
     'WHERE id = $9 ' +
-    'RETURNING *'
+    'RETURNING *';
 
     const values = [username, name, address, city, postalcode, mobilephone, email, roleId, id];
     const result = await client.query(query, values);
@@ -72,18 +72,18 @@ class PostgreUserRepository {
   }
   
   async delete (user) {
-    const { id, roleId } = user
+    let { id, role_id } = user
 
     const client = new pg.Client(this.baseURI)
     await client.connect()
+    
+    const role_admin = 1;
 
-    const roleAdmin = 1
-
-    if (roleId === roleAdmin) // Admin
+    if ( role_id == role_admin ) //Admin
     {
-      const roleq = await client.query('SELECT count(*) FROM users WHERE role_id = $1', [roleAdmin]) // Admin
-      console.log('ID: ' + roleq.rows[0][0] + ' - CNT: ' + roleq.rows[0][2])
-      if(roleq.rows[0][0] <= 1) {
+      const roleq = await client.query('SELECT count(*) FROM users WHERE role_id = $1', [role_admin]) // Admin
+      console.log( "ID: " + roleq.rows[0][0] + " - CNT: " + roleq.rows[0][2] );
+      if( roleq.rows[0][0] <= 1 ) {
           console.log('Last admin user cannot be removed')
           return ({ Message: "Cannot remove last admin" });
       } else {
