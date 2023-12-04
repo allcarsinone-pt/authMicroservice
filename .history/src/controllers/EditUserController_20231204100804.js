38f164639fd74e4da17ad18a8262d67a0c834ff7
pestevao <pestevao@sdilab.pt>
2023-12-04 15:32:54 +0000

    Correções no front-end

---
+++ .history/src/controllers/EditUserController_20231204100804.js
@@ -0,0 +1,70 @@
+const jwt = require('jsonwebtoken')
+const EditUserUseCase = require('../usecases/EditUserUseCase/EditUser.usecase')
+const ValidateAuthUseCase = require('../usecases/ValidateAuthUseCase/ValidateAuth.usecase')
+const LogService = require('./services/LogService')
+// Acoplado com o express. O req e o res têm de estar aqui ou não vale a pena complicar ?- perguntar ao professor de arquitetura
+
+/**
+ * @class EditUserController
+ * @description Controller to edit an user
+ */
+
+class EditUserController {
+  constructor (userRepository, logService) {
+    this.userRepository = userRepository
+    this.secret = secret
+    this.logService = logService
+  }
+
+  /**
+   * @description Method to execute http request to edit an user
+   * @param {*} request request object from express
+   * @param {*} response response object from express
+   * @returns response object from express
+   */
+
+  async execute (request, response) {
+    if (!request.headers.authorization) {
+      return response.status(401).json({ error: 'No token provided' })
+    }
+    // Verify token to get user profile
+    try {
+      const token = request.headers.authorization.split(' ')[1]
+      const userAuth = jwt.verify(token, this.secret)
+      const validateAuthUseCase = new ValidateAuthUseCase(this.userRepository)
+      const resultAUth = await validateAuthUseCase.execute(userAuth)
+      if (!resultAUth.success) {
+        LogService.execute({ from: 'authEditService', data: resultAUth.error.message, date: new Date(), status: 'error' }, this.logService)
+        return response.status(500).json({ error: resultAUth.error.message })
+      }
+
+
+      const { resultAUth.data.role_id, username, name, email, role_id } = request.body
+      if (!id || !email || !username || !name || !role_id) {
+        await LogService.execute({ from: 'authEditService', data: 'Missing fields', date: new Date(), status: 'error' }, this.logService)
+        return response.status(400).json({ message: 'Missing fields' })
+      }
+
+      const usecase = new EditUserUseCase(this.userRepository)
+      const user = await usecase.execute({
+        id, username, name, address, city, postalcode, mobilephone, email, role_id
+      })
+
+      if (!user.success) {
+        await LogService.execute({ from: 'authEditService', data: `${user.error.message}`, date: new Date(), status: 'error' }, this.logService)
+        if (user.error.message === 'Email already used' || user.error.message === 'Name is required' || user.error.message === 'Invalid user') {
+          return response.status(400).json({ message: user.error.message })
+        } else {
+          return response.status(500).json({ message: 'Internal server error' })
+        }
+      }
+      await LogService.execute({ from: 'authEditService', data: `${user.data.id}-${user.data.role_id} edited`, date: new Date(), status: 'info' }, this.logService)
+      return response.status(201).json(user.data)
+    } catch (error) {
+      await LogService.execute({ from: 'authEditService', data: error.message, date: new Date(), status: 'error' }, this.logService)
+      return response.status(401).json({ error: error.message })
+    }
+  }
+}
+
+module.exports = EditUserController
---
+++ .history/src/usecases/UsersUseCase/Users.usecase_20231204124936.js
@@ -0,0 +1,25 @@
+const { handleError, Result } = require('../../util/Result')
+
+/**
+ * @description Login use case of application
+ */
+class LoginUseCase {
+  constructor (userRepository) {
+    this.userRepository = userRepository
+  }
+
+  /**
+     * @description verifies if credentials are correct and returns an user object
+     * @param {*} loginDto email and password of user
+     */
+  async execute (loginDto, hashAlgorithm) {
+    const withErrorHandling = handleError(async () => {
+      const result = await this.userRepository.loadAllUsers()
+      console.log(result)
+      return Result.success(result)
+    })
+    return withErrorHandling()
+  }
+}
+
+module.exports = LoginUseCase
