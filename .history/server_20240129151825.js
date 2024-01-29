4e50b25e5f5764bf89ac063cc19edfbd8f5deb30
pestevao <pestevao@sdilab.pt>
2024-01-29 17:10:26 +0000

    Implementação do http2.0

---
+++ .history/server_20240129151825.js
@@ -0,0 +1,42 @@
+const makeApp = require('./src/appBuilder')
+const dotenv = require('dotenv')
+const swaggerDocument = require('./docs/swagger')
+const swaggerUi = require('swagger-ui-express')
+const PostgreUserRepository = require('./src/repositories/PostgreUserRepository')
+const RabbitMQLogAdapter = require('./src/adapters/RabbitMQLogAdapter')
+const path = require('path')
+const bodyParser = require('body-parser')
+const express = require('express')
+const ElasticLogService = require('./src/controllers/services/ElasticLogService')
+const LogMockAdapter = require('./src/adapters/LogMockAdapter')
+dotenv.config()
+
+// new LogMockAdapter())
+//const app = makeApp(new PostgreUserRepository(process.env.DATABASE_URL), new RabbitMQLogAdapter(process.env.RABBITMQ_URL))
+
+const app = makeApp(new PostgreUserRepository(process.env.DATABASE_URL), new LogMockAdapter())
+console.log(process.env.DATABASE_URL)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+// Midleware
+app.use('/', express.static(path.join(__dirname, 'src/static')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.listen(process.env.SERVER_PORT || 3001, () => {
+//  console.log(`Server is running on http://localhost:${process.env.SERVER_PORT || 3001}/`)
+//})
+
+
+
+
+
+const https = require('https')
+const fs = require('fs')
+
+const options = {
+  key: fs.readFileSync('path/to/private-key.pem'),
+  cert: fs.readFileSync('path/to/certificate.pem')
+}
+const server = https.createServer(options, app)
+server.listen(process.env.SERVER_PORT || 3001, () => {
+  console.log(`Server is running on http://localhost:${process.env.SERVER_PORT || 3001}/`)
+})
